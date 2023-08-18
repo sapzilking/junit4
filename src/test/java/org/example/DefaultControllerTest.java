04931e0c29c447da9c838a3fb785c7d17dd3cd6f
sapzilking <sapzilking@gmail.com>
2023-08-18 22:54:26 +0900

    processRequest 테스트코드 작성

--- src/test/java/org/example/DefaultControllerTest.java
+++ src/test/java/org/example/DefaultControllerTest.java
@@ -7,24 +7,15 @@ import static org.junit.Assert.*;
 
 public class DefaultControllerTest {
     private DefaultController controller;
+    private Request request;
+    private RequestHandler handler;
 
     @Before
-    public void instantiate() {
+    public void initialize() {
         controller = new DefaultController();
-    }
-
-    @Test
-    public void testMethod() {
-        throw new RuntimeException("implement me");
-    }
-
-    @Test
-    public void testAddHandler() {
-        Request request = new SampleRequest();
-        RequestHandler handler = new SampleHandler();
+        request = new SampleRequest();
+        handler = new SampleHandler();
         controller.addHandler(request, handler);
-        RequestHandler handler2 = controller.getHandler(request);
-        assertSame("Handler we set in controller should be the same handler we get", handler2, handler);
     }
 
     private class SampleRequest implements Request {
@@ -42,4 +33,21 @@ public class DefaultControllerTest {
 
     private class SampleResponse implements Response {
     }
+
+    @Test
+    public void testAddHandler() {
+        RequestHandler handler2 = controller.getHandler(request);
+        assertSame("Handler we set in controller should be the same handler we get", handler2, handler);
+    }
+
+    @Test
+    public void testProcessRequest() {
+        Response response = controller.processRequest(request);
+        assertNotNull("Must not return a null response", response);
+        assertEquals("Response should be of type SampleResponse", SampleResponse.class, response.getClass());
+    }
+
+
+
+
 }
